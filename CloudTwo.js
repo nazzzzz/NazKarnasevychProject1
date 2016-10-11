67f5d18891cfeb7e6b0d246bddb70f2d1c632b8a
nazzzzz <nk1721@nyu.edu>
2016-10-10 21:19:04 -0400

    finished project

---
+++ CloudTwo.js
@@ -0,0 +1,71 @@
+//class for second type of cloud
+function CloudTwo(cloudX, cloudY) {
+
+    //defining parameters
+    this.xPos = cloudX;
+    this.yPos = cloudY;
+    this.speed = random(0.5, 1);
+    this.faded = 0;
+    this.cloud_dark = color(237, 179, 167);
+    this.cloud_light = color(247, 208, 207);
+
+    //display function to draw the different ellipses that make up the cloud
+    this.display = function() {
+        noStroke();
+        fill(this.cloud_light);
+        ellipse(this.xPos + 25, this.yPos - 20, 40, 40);
+        ellipse(this.xPos - 25, this.yPos - 15, 30, 20);
+
+        fill(this.cloud_dark);
+        ellipse(this.xPos, this.yPos, 60, 60);
+        ellipse(this.xPos + 60, this.yPos + 5, 30, 30);
+        ellipse(this.xPos + 65, this.yPos + 20, 30, 30);
+        ellipse(this.xPos + 45, this.yPos - 10, 40, 30);
+        ellipse(this.xPos - 40, this.yPos + 10, 50, 40);
+
+
+        fill(this.cloud_light);
+        ellipse(this.xPos - 5, this.yPos + 10, 15, 15);
+        ellipse(this.xPos + 55, this.yPos + 3, 25, 25);
+
+        fill(this.cloud_dark);
+        ellipse(this.xPos, this.yPos + 30, 150, 20);
+        ellipse(this.xPos + 30, this.yPos + 10, 60, 40);
+        ellipse(this.xPos + 70, this.yPos + 15, 60, 30);
+
+    }
+
+    //move function that changes the cloud's position by adding its speed to the position
+    this.move = function() {
+        if (this.xPos - 70 > width) {
+            this.xPos = -200;
+        }
+        this.xPos = this.xPos + this.speed;
+    }
+
+    //function that slows down the cloud by lowering its speed
+    this.slow = function() {
+        {
+            if (this.speed > 0) {
+                this.speed = this.speed - 0.005;
+            } else {
+                this.speed = 0;
+            }
+        }
+    }
+
+     //this function fades the cloud
+    this.fade = function() {
+        //if the cloud is done moving
+        if (this.speed == 0) {
+            //updates the faded amount, and makes sure that it doesn't overflow
+            if (this.faded <= 1) {
+                this.faded += 0.001;
+            }
+            this.cloud_dark = lerpColor(color(237, 179, 167), color(255, 245, 227), this.faded);
+            this.cloud_light = lerpColor(color(247, 208, 207), color(255, 245, 227), this.faded);
+
+        }
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,60 +1,158 @@
+//arrays to store the clouds and a finished bool to check if the background is done fading out.
+
+var cloudOneArray = [];
+var cloudTwoArray = [];
+finished = false;
 
 function setup() {
     createCanvas(2000, 1400);
+    background(255);
 
-    //sunset color
-    background(145, 34, 25, 60);
-    noStroke();
-    //grass
-    fill(10, 230, 20, 70);
-    rect(0,1000, 2000, 1000);
-    //sky
-    fill(70, 80, 230);
-    rect(0,0, 2000, 500);
+    //fill arrays with clouds positioned randomly between a select range
+    for (i = 0; i < 3; i++) {
+        cloudOneArray[i] = new CloudOne(random(0, 2000), random(500, 700));
+    }
+
+
+    for (i = 0; i < 6; i++) {
+        cloudTwoArray[i] = new CloudTwo(random(0, 2000), random(700, 850));
+    }
 }
 
 
 
 function draw() {
-	 
-    drawTree();
+
+    //drawing the background and the sun
+    drawBackground();
     drawSun();
-}
 
-
-function drawTree() {
-
-	//TRUNK
-	trunkVec = [];
-    for (counter = 900; counter < 1440; counter += 10) {
-        trunkVec.push({x:1500, y:counter});
-        
+    //drawing the clouds and telling them to move
+    for (i = 0; i < cloudOneArray.length; i++) {
+        cloudOneArray[i].move();
+        cloudOneArray[i].display();
     }
 
-    for (i = 0; i < trunkVec.length; i++) {
-    	fill(0);
-    	//println(trunkVec[i].x, trunkVec[i].y);
-        ellipse(trunkVec[i].x, trunkVec[i].y, 100, 40);
+    for (i = 0; i < cloudTwoArray.length; i++) {
+        cloudTwoArray[i].move();
+        cloudTwoArray[i].display();
     }
 
-    //Branch 1
 
 
 }
 
-function drawSun(){
-	fill(200, 150, 20, 50);
-	rect(200, 600, 150, 150);
+function drawSun() {
+    //outer rect
+    fill(200, 150, 20, 75);
+    rect(550, 900, 250, 55);
 
-	fill(250, 170, 0, 50);
-	rect(225, 625, 100, 100);
+    //middle rect
+    fill(250, 170, 0, 75);
+    rect(575, 900, 200, 45);
 
+    //inner rect
+    fill(255, 170, 0);
+    rect(600, 900, 150, 30);
 
-	fill(255, 170, 0);
-	rect(250, 650, 50, 50);
-	
 }
 
-function drawCloud(params){
 
+function drawBackground() {
+
+    //if the framecount is almost 1000, set the finished bool to true so that the colors don't reset
+    if (frameCount % 1000 == 999) {
+        finished = true;
+    }
+
+    //defining colors for the different areas
+    sky_upper = color(140, 113, 102);
+    sky_lower = color(253, 197, 162);
+    water_upper = color(227, 105, 117);
+    water_lower = color(197, 105, 115);
+    grass_upper = color(164, 124, 37);
+    grass_lower = color(130, 124, 38);
+    grass_other = color(219, 191, 66);
+    finishedColor = color(255, 245, 227);
+
+    //if the framecount has not reached 1000, keep changing each of the background areas towards the finished color.
+    if (!finished) {
+        sky_upper = lerpColor(sky_upper, finishedColor, frameCount % 1000 / 1000);
+        sky_lower = lerpColor(sky_lower, finishedColor, frameCount % 1000 / 1000);
+        grass_lower = lerpColor(grass_lower, finishedColor, frameCount % 1000 / 1000);
+        grass_upper = lerpColor(grass_upper, finishedColor, frameCount % 1000 / 1000);
+        water_lower = lerpColor(water_lower, finishedColor, frameCount % 1000 / 1000);
+        water_upper = lerpColor(water_upper, finishedColor, frameCount % 1000 / 1000);
+        grass_other = lerpColor(grass_other, finishedColor, frameCount % 1000 / 1000);
+
+    }
+    //when it is finished, permanently set the colors of the background to the finished color
+    else {
+        sky_upper = finishedColor;
+        sky_lower = finishedColor;
+        grass_lower = finishedColor;
+        grass_upper = finishedColor;
+        water_lower = finishedColor;
+        water_upper = finishedColor;
+        grass_other = finishedColor;
+
+        //also, when finished changing the background, gradually stop the clouds and fade them out
+        for (i = 0; i < cloudOneArray.length; i++) {
+            cloudOneArray[i].slow();
+            cloudOneArray[i].fade();
+        }
+
+        for (i = 0; i < cloudTwoArray.length; i++) {
+            cloudTwoArray[i].slow();
+            cloudTwoArray[i].fade();
+        }
+
+    }
+
+    ////////////////////////////
+    //drawing the different areas
+    ////////////////////////////
+
+
+    //sky upper
+    fill(sky_upper);
+    rect(0, 0, 2000, 500);
+
+    //sky Lower
+    gradientColor(0, 500, 2000, 390, sky_upper, sky_lower);
+
+
+    //line of white ish
+    gradientColor(0, 890, 2000, 10, color(251, 219, 194), color(249, 236, 227));
+
+
+    //water
+    gradientColor(0, 900, 2000, 100, water_upper, water_lower);
+
+
+    //grass
+	gradientColor(0, 1050, 2000, 175, grass_upper, grass_lower);
+
+    noStroke();
+    fill(grass_lower)
+    rect(0, 1225, 2000, 175);
+
+    //grass 2.0 
+    fill(grass_other);
+    rect(0, 1000, 2000, 50);
+
+
+
+}
+
+//linear gradient function found on Processing referenece, modified for p5 and one axis
+//basically draws lines down the screen, with ever subtly changing colors
+function gradientColor(x, y, w, h, c1, c2) {
+
+    for (i = y; i <= y + h; i++) {
+        inter = map(i, y, y + h, 0, 1);
+        c = lerpColor(c1, c2, inter);
+        stroke(c);
+        line(x, i, x + w, i);
+    }
 }
